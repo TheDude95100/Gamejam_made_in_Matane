41b4430c4ce60b06d0194ba8dc45fe96df8de5ef
2001085 <hcalvet70@gmail.com>
2024-03-14 13:57:04 -0400

    Refactor du systeme de dialogue

--- Lies_isolated_struggle/Assets/Scripts/Dialogue/CompagnonDialogue.cs
+++ Lies_isolated_struggle/Assets/Scripts/Dialogue/CompagnonDialogue.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
@@ -8,17 +9,15 @@ using UnityEngine;
 public class CompagnonDialogue : MonoBehaviour
 {
     public Dialogue[] listeDialogue;
-
-    public Dialogue introduction;
-
+    public string nomPersonnage;
 
     public Dictionary<string, Dialogue> dictionnaireDialogue;
-
-    public Dialogue dialogueFin;
 
     public Dictionary<string, string> dictionnaireEvent;
 
     private DialogueManager _dm;
+
+    public TextMeshProUGUI textBouton;
 
     public bool flagEvent = false;
     public bool DialogueFini { get; set; }
@@ -26,22 +25,17 @@ public class CompagnonDialogue : MonoBehaviour
     private void Start()
     {
         _dm = FindObjectOfType<DialogueManager>();
+        textBouton.text = nomPersonnage;
         dictionnaireDialogue = new Dictionary<string, Dialogue>();
-        dialogueFin.nom = introduction.nom;
-        dictionnaireDialogue.Add("Introduction", introduction);
         dictionnaireEvent= new Dictionary<string, string>()
         {
-            {"Mort", "Nom du mort"},
-            {"Loot", "Loot"}
+           {"Mort", "Nom du mort"},
+           // {"Loot", "Loot"}
             //{"Event", new Dialogue(introduction.nom, "Test")}
         };
         for (int indexDialogue = 0; indexDialogue < listeDialogue.Length; indexDialogue++)
         {
             dictionnaireDialogue.Add(listeDialogue[indexDialogue].nom, listeDialogue[indexDialogue]);
-        }
-        foreach(KeyValuePair<string,Dialogue> entry in dictionnaireDialogue)
-        {
-            Debug.Log(entry.Key);
         }
     }
 
@@ -51,12 +45,11 @@ public class CompagnonDialogue : MonoBehaviour
         if (dictionnaireEvent.Count > 0)
         {
             _dm.currentDialogue = dictionnaireEvent.First().Key;
-            _dm.StartDialogue(new Dialogue(introduction.nom, dictionnaireDialogue[dictionnaireEvent.First().Key].phrases));
-            dictionnaireEvent.Remove(dictionnaireEvent.First().Key);
+            _dm.StartDialogue(new Dialogue(nomPersonnage, dictionnaireDialogue[dictionnaireEvent.First().Key].phrases));
             return;
         }
-        if(DialogueFini) {_dm.StartDialogue(dialogueFin); return; }
-        _dm.StartDialogue(new Dialogue(introduction.nom,dictionnaireDialogue[dialogue].phrases));
+        if(DialogueFini) {_dm.StartDialogue(new Dialogue(nomPersonnage, dictionnaireDialogue["Dialogue Fin"].phrases)); return; }
+        _dm.StartDialogue(new Dialogue(nomPersonnage,dictionnaireDialogue[dialogue].phrases));
         _dm.currentDialogue = dialogue;
     }   
 }
--- Lies_isolated_struggle/Assets/Scripts/Dialogue/ReponseButton.cs
+++ Lies_isolated_struggle/Assets/Scripts/Dialogue/ReponseButton.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
 
 public class ReponseButton : MonoBehaviour
@@ -10,12 +12,23 @@ public class ReponseButton : MonoBehaviour
 
     public int idButton;
 
+    public Dialogue[] listeReponse;
+
+    Dictionary<string, Dialogue> _reponseDic;
+
+    public TextMeshProUGUI textMeshPro;
+
     private void OnEnable()
     {
         _dm = FindObjectOfType<DialogueManager>();
         TalkingCharacter = _dm.talkingCompagnon;
-        
-       
+        string dialogue = _dm.currentDialogue;
+        _reponseDic= new Dictionary<string, Dialogue>();
+        for (int indexDialogue = 0; indexDialogue < listeReponse.Length; indexDialogue++)
+        {
+            _reponseDic.Add(listeReponse[indexDialogue].nom, listeReponse[indexDialogue]);
+        }
+        textMeshPro.text = _reponseDic[dialogue].phrases[0];
     }
     public void TriggerReponse()
     {
@@ -25,7 +38,8 @@ public class ReponseButton : MonoBehaviour
             TalkingCharacter.DialogueFini = true;
         }
         _dm.chosenAnswer = dialogue + " " + idButton;
-        _dm.StartDialogue(new Dialogue(TalkingCharacter.introduction.nom, TalkingCharacter.dictionnaireDialogue[_dm.currentDialogue + " " + idButton].phrases));
+        _dm.StartDialogue(new Dialogue(TalkingCharacter.nomPersonnage, TalkingCharacter.dictionnaireDialogue[_dm.currentDialogue + " " + idButton].phrases));
+        if(_dm.talkingCompagnon.dictionnaireEvent.Count >0) { _dm.talkingCompagnon.dictionnaireEvent.Remove(_dm.talkingCompagnon.dictionnaireEvent.First().Key); }
         _dm.talkingCompagnon.flagEvent= true;
     }
 }
