b815e5e23dec0dd3161fc62b9db911f5e33f6262
igatia <war_mai@hotmail.com>
2024-03-15 14:05:49 -0400

    Début Item Créator

--- Lies_isolated_struggle/Assets/test_warren/ItemCreator/ItemData.cs
+++ Lies_isolated_struggle/Assets/test_warren/ItemCreator/ItemData.cs
@@ -2,17 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemData : MonoBehaviour
+[CreateAssetMenu(fileName = "ItemData_", menuName = "Datasheet/Item")]
+public class ItemData : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private string _name = "...";
+    [SerializeField]
+    private ItemType _type = ItemType.None;
+    [SerializeField]
+    [TextArea(1,4)]
+    private string _description = "...";
+    [SerializeField]
+    private bool _isEquipped = false;
+    [SerializeField]
+    private int _quantity = 0;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField]
+    private bool _isAWeapon = false;
+    [SerializeField]
+    private int _damage = 0;
+    [SerializeField]
+    private int _range = 0;
+    [SerializeField]
+    private float _accuracy = 0.5f;
+
+    [SerializeField]
+    private bool _isAnArmor = false;
+    [SerializeField]
+    private int _defense = 0;
+
+    [SerializeField]
+    private bool _isAConsumable = false;
+
+    [SerializeField]
+    private bool _isAResosurce = false;
+    [SerializeField]
+    private int _food = 0;
+    [SerializeField]
+    private int _water = 0;
 }
