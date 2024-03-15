c93ade04d07b03dde6777d3610cea31c94eb0d35
2001085 <hcalvet70@gmail.com>
2024-03-15 13:31:38 -0400

    Affinite modulaire #4

--- Lies_isolated_struggle/Assets/Scripts/Dialogue/CompagnonDialogue.cs
+++ Lies_isolated_struggle/Assets/Scripts/Dialogue/CompagnonDialogue.cs
@@ -9,29 +9,40 @@ using UnityEngine;
 public class CompagnonDialogue : MonoBehaviour
 {
     public Dialogue[] listeDialogue;
-    public Dialogue[] listeReponse;
+
+    public Reponse[] listeReponse;
+
     public string nomPersonnage;
 
     public Dictionary<string, Dialogue> dictionnaireDialogue;
 
     public Dictionary<string, string> dictionnaireEvent;
 
-    public Dictionary<string, Dialogue> dictionnaireReponse;
+    public Dictionary<string, Reponse> dictionnaireReponse;
 
     private DialogueManager _dm;
 
     public TextMeshProUGUI textBouton;
 
+    public int affinite;
+
     public bool flagEvent = false;
+
+    public int affiniteSurvie;
+    public int affiniteCombat;
+    public int affiniteAnimaux;
+
+    private int[] _matriceAffinite;
     public bool DialogueFini { get; set; }
 
     private void Start()
     {
         _dm = FindObjectOfType<DialogueManager>();
         textBouton.text = nomPersonnage;
+        _matriceAffinite = new int[3];
         dictionnaireDialogue = new Dictionary<string, Dialogue>();
-        dictionnaireReponse= new Dictionary<string, Dialogue>();
-        dictionnaireEvent= new Dictionary<string, string>()
+        dictionnaireReponse = new Dictionary<string, Reponse>();
+        dictionnaireEvent = new Dictionary<string, string>()
         {
            {"Mort", "Nom du mort"},
            // {"Loot", "Loot"}
@@ -45,6 +56,7 @@ public class CompagnonDialogue : MonoBehaviour
         {
             dictionnaireReponse.Add(listeReponse[indexDialogue].nom, listeReponse[indexDialogue]);
         }
+        creerMatriceAffinite();
     }
 
     public void TriggerDialogue(string dialogue)
@@ -56,8 +68,20 @@ public class CompagnonDialogue : MonoBehaviour
             _dm.StartDialogue(new Dialogue(nomPersonnage, dictionnaireDialogue[dictionnaireEvent.First().Key].phrases));
             return;
         }
-        if(DialogueFini) {_dm.StartDialogue(new Dialogue(nomPersonnage, dictionnaireDialogue["Dialogue Fin"].phrases)); return; }
-        _dm.StartDialogue(new Dialogue(nomPersonnage,dictionnaireDialogue[dialogue].phrases));
+        if (DialogueFini) { _dm.StartDialogue(new Dialogue(nomPersonnage, dictionnaireDialogue["Dialogue Fin"].phrases)); return; }
+        _dm.StartDialogue(new Dialogue(nomPersonnage, dictionnaireDialogue[dialogue].phrases));
         _dm.currentDialogue = dialogue;
-    }   
+    }
+
+    private void creerMatriceAffinite()
+    {
+        _matriceAffinite[0] = affiniteSurvie;
+        _matriceAffinite[1] = affiniteCombat;
+        _matriceAffinite[2] = affiniteAnimaux;
+    }
+
+    public int[] GetMatriceAffinite()
+    {
+        return _matriceAffinite;
+    }
 }
