dc12a7f1bcd06377c160ebcc9afaa749635454d1
igatia <war_mai@hotmail.com>
2025-05-10 17:10:15 -0400

    mouse world position
    - removed game object grids
    - properly scaled voxels
    - added simple post processing

--- Lies_isolated_struggle/Assets/test_warren/Script/MouseWorld.cs
+++ Lies_isolated_struggle/Assets/test_warren/Script/MouseWorld.cs
@@ -4,11 +4,27 @@ using UnityEngine;
 
 public class MouseWorld : MonoBehaviour
 {
+    private static MouseWorld _instance;
+
+    [SerializeField]
+    private LayerMask groundLayerMask;
+
+    private void Awake()
+    {
+        _instance = this;
+    }
     // Update is called once per frame
     void Update()
     {
+        transform.position = MouseWorld.GetPosition();
+    }
+
+    public static Vector3 GetPosition()
+    {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-        Debug.Log(Physics.Raycast(ray));
+        Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, _instance.groundLayerMask);
+
+        return raycastHit.point;
     }
 }
